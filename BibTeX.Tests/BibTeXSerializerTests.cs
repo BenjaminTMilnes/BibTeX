86566ba26286432f2418c49fda7a14d6688cf2ae
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-03-19 18:08:57 +0000

    Established unit testing of BibTeXSerializer

---
+++ BibTeX.Tests/BibTeXSerializerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BibTeX;
+
+namespace BibTeX.Tests
+{
+    [TestClass]
+    public class BibTeXSerializerTests
+    {
+        private BibTeXSerializer _serializer;
+
+        public BibTeXSerializerTests()
+        {
+            _serializer = new BibTeXSerializer();
+        }
+
+        [TestMethod]
+        public void GetBibTeXEntryNameTest()
+        {
+            Assert.AreEqual("book", _serializer.GetBibTeXEntryName(_serializer.GetBibTeXEntryType(new BibTeXBook())));
+        }
+    }
+}
