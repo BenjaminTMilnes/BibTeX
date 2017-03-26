c3c8370b0cddbf62430e16ed5e056aeaacf49e62
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2017-03-26 10:06:36 +0100

    Added BibTeXBooklet

---
+++ BibTeX/BibTeXBooklet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibTeX
+{
+    [BibTeXEntryName("booklet")]
+    public class BibTeXBooklet : BibTeXEntry
+    {
+        [BibTeXFieldName("title")]
+        public string Title { get; set; }
+
+        [BibTeXFieldName("author")]
+        [BibTeXOptionalField]
+        public string Author { get; set; }
+
+        [BibTeXFieldName("howpublished")]
+        [BibTeXOptionalField]
+        public string HowPublished { get; set; }
+
+        [BibTeXFieldName("address")]
+        [BibTeXOptionalField]
+        public string Address { get; set; }
+
+        [BibTeXFieldName("month")]
+        [BibTeXOptionalField]
+        public BibTeXMonth Month { get; set; }
+
+        [BibTeXFieldName("year")]
+        [BibTeXOptionalField]
+        public string Year { get; set; }
+
+        [BibTeXFieldName("note")]
+        [BibTeXOptionalField]
+        public string Note { get; set; }
+
+        [BibTeXFieldName("key")]
+        [BibTeXOptionalField]
+        public string Key { get; set; }
+
+        public BibTeXBooklet() { }
+
+        public BibTeXBooklet(string title)
+        {
+            Title = title;
+        }
+    }
+}
