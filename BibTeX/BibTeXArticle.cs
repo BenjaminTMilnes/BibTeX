0c81859d48ef4201700e934fc46099b85368cc20
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2017-03-26 09:57:20 +0100

    Added BibTeXArticle

---
+++ BibTeX/BibTeXArticle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibTeX
+{
+    [BibTeXEntryName("article")]
+    public class BibTeXArticle : BibTeXEntry
+    {
+        [BibTeXFieldName("author")]
+        public string Author { get; set; }
+
+        [BibTeXFieldName("title")]
+        public string Title { get; set; }
+
+        [BibTeXFieldName("journal")]
+        public string Journal { get; set; }
+
+        [BibTeXFieldName("year")]
+        public string Year { get; set; }
+
+        [BibTeXFieldName("volume")]
+        public string Volume { get; set; }
+
+        [BibTeXFieldName("number")]
+        [BibTeXOptionalField]
+        public string Number { get; set; }
+
+        [BibTeXFieldName("pages")]
+        [BibTeXOptionalField]
+        public string Pages { get; set; }
+
+        [BibTeXFieldName("month")]
+        [BibTeXOptionalField]
+        public BibTeXMonth Month { get; set; }
+
+        [BibTeXFieldName("note")]
+        [BibTeXOptionalField]
+        public string Note { get; set; }
+
+        [BibTeXFieldName("key")]
+        [BibTeXOptionalField]
+        public string Key { get; set; }
+
+        public BibTeXArticle() { }
+
+        public BibTeXArticle(string author, string title, string journal, string year, string volume)
+        {
+            Author = author;
+            Title = title;
+            Journal = journal;
+            Year = year;
+            Volume = volume;
+        }
+    }
+}
