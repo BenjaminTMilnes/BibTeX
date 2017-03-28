2f5556454502983bea4d0256821eae278bb8c539
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-03-28 17:49:11 +0100

    Added BibTeXInBook

---
+++ BibTeX/BibTeXInBook.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibTeX
+{
+    [BibTeXEntryName("inbook")]
+    public class BibTeXInBook : BibTeXEntry
+    {
+        [BibTeXFieldName("author")]
+        public string Author { get; set; }
+
+        [BibTeXFieldName("title")]
+        public string Title { get; set; }
+
+        [BibTeXFieldName("chapter")]
+        public string Chapter { get; set; }
+
+        [BibTeXFieldName("publisher")]
+        public string Publisher { get; set; }
+
+        [BibTeXFieldName("year")]
+        public string Year { get; set; }
+
+        [BibTeXFieldName("volume")]
+        [BibTeXOptionalField]
+        public string Volume { get; set; }
+
+        [BibTeXFieldName("series")]
+        [BibTeXOptionalField]
+        public string Series { get; set; }
+
+        [BibTeXFieldName("type")]
+        [BibTeXOptionalField]
+        public string Type { get; set; }
+
+        [BibTeXFieldName("address")]
+        [BibTeXOptionalField]
+        public string Address { get; set; }
+
+        [BibTeXFieldName("edition")]
+        [BibTeXOptionalField]
+        public string Edition { get; set; }
+
+        [BibTeXFieldName("month")]
+        [BibTeXOptionalField]
+        public BibTeXMonth Month { get; set; }
+
+        public BibTeXInBook() { }
+
+        public BibTeXInBook(string author, string title, string chapter, string publisher, string year)
+        {
+            Author = author;
+            Title = title;
+            Chapter = chapter;
+            Publisher = publisher;
+            Year = year;
+        }
+    }
+}
