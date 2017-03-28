b4869be30f9fe1f5742c06ea87951d4729ab8c12
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-03-28 18:20:49 +0100

    Added BibTeXManual

---
+++ BibTeX/BibTeXManual.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibTeX
+{
+    [BibTeXEntryName("manual")]
+    public class BibTeXManual : BibTeXEntry
+    {
+        [BibTeXFieldName("title")]
+        public string Title { get; set; }
+
+        [BibTeXFieldName("author")]
+        [BibTeXOptionalField]
+        public string Author { get; set; }
+
+        [BibTeXFieldName("organization")]
+        [BibTeXOptionalField]
+        public string Organization { get; set; }
+
+        [BibTeXFieldName("address")]
+        [BibTeXOptionalField]
+        public string Address { get; set; }
+
+        [BibTeXFieldName("edition")]
+        [BibTeXOptionalField]
+        public string Edition { get; set; }
+
+        [BibTeXFieldName("month")]
+        [BibTeXOptionalField]
+        public BibTeXMonth Month { get; set; }
+
+        [BibTeXFieldName("year")]
+        [BibTeXOptionalField]
+        public string Year { get; set; }
+
+        public BibTeXManual() { }
+
+        public BibTeXManual(string title)
+        {
+            Title = title;
+        }
+    }
+}
