dc2c759e06c0c5d3bdf51f3fa541b09a5c51fd86
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-03-29 18:51:35 +0100

    Added BibTeXPhDThesis

---
+++ BibTeX/BibTeXPhDThesis.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibTeX
+{
+    [BibTeXEntryName("phdthesis")]
+    public class BibTeXPhDThesis : BibTeXEntry
+    {
+        [BibTeXFieldName("author")]
+        public string Author { get; set; }
+
+        [BibTeXFieldName("title")]
+        public string Title { get; set; }
+
+        [BibTeXFieldName("school")]
+        public string School { get; set; }
+
+        [BibTeXFieldName("year")]
+        public string Year { get; set; }
+
+        [BibTeXFieldName("type")]
+        [BibTeXOptionalField]
+        public string Type { get; set; }
+
+        [BibTeXFieldName("address")]
+        [BibTeXOptionalField]
+        public string Address { get; set; }
+
+        [BibTeXFieldName("month")]
+        [BibTeXOptionalField]
+        public BibTeXMonth Month { get; set; }
+
+        public BibTeXPhDThesis() { }
+
+        public BibTeXPhDThesis(string author, string title, string school, string year)
+        {
+            Author = author;
+            Title = title;
+            School = school;
+            Year = year;
+        }
+    }
+}
