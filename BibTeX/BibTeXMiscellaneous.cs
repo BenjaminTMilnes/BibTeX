2a3eaa4a8b29b3da826c46051d8d33cabb7dd0a5
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-03-29 18:45:31 +0100

    Added BibTeXMiscellaneous

---
+++ BibTeX/BibTeXMiscellaneous.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibTeX
+{
+    [BibTeXEntryName("misc")]
+    public class BibTeXMiscellaneous : BibTeXEntry
+    {
+        [BibTeXFieldName("author")]
+        [BibTeXOptionalField]
+        public string Author { get; set; }
+
+        [BibTeXFieldName("title")]
+        [BibTeXOptionalField]
+        public string Title { get; set; }
+
+        [BibTeXFieldName("howpublished")]
+        [BibTeXOptionalField]
+        public string HowPublished { get; set; }
+
+        [BibTeXFieldName("year")]
+        [BibTeXOptionalField]
+        public string Year { get; set; }
+
+        [BibTeXFieldName("month")]
+        [BibTeXOptionalField]
+        public BibTeXMonth Month { get; set; }
+
+        public BibTeXMiscellaneous() { }
+    }
+}
