a3af1a8e60daf29385b9bfca0cf40bc04fca9d29
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-03-30 20:22:08 +0100

    Added BibTeXTechReport

---
+++ BibTeX/BibTeXTechReport.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibTeX
+{
+    [BibTeXEntryName("techreport")]
+    public class BibTeXTechReport : BibTeXEntry
+    {
+        [BibTeXFieldName("author")]
+        public string Author { get; set; }
+
+        [BibTeXFieldName("title")]
+        public string Title { get; set; }
+
+        [BibTeXFieldName("institution")]
+        public string Institution { get; set; }
+
+        [BibTeXFieldName("year")]
+        public string Year { get; set; }
+
+        [BibTeXFieldName("type")]
+        [BibTeXOptionalField]
+        public string Type { get; set; }
+
+        [BibTeXFieldName("number")]
+        [BibTeXOptionalField]
+        public string Number { get; set; }
+
+        [BibTeXFieldName("address")]
+        [BibTeXOptionalField]
+        public string Address { get; set; }
+
+        [BibTeXFieldName("month")]
+        [BibTeXOptionalField]
+        public BibTeXMonth Month { get; set; }
+
+        public BibTeXTechReport() { }
+
+        public BibTeXTechReport(string author, string title, string institution, string year)
+        {
+            Author = author;
+            Title = title;
+            Institution = institution;
+            Year = year;
+        }
+    }
+}
