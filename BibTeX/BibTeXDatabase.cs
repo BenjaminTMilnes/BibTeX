fc60ee3bbd64dfe1c9c078aaaadea22fdf9ae0c8
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-03-30 20:36:29 +0100

    Added BibTeXDatabase

---
+++ BibTeX/BibTeXDatabase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibTeX
+{
+    public class BibTeXDatabase
+    {
+        public IList<IBibTeXEntry> Entries { get; set; }
+
+        public BibTeXDatabase()
+        {
+            Entries = new List<IBibTeXEntry>();
+        }
+
+        #region GetEntriesByType
+
+        public IEnumerable<IBibTeXEntry> GetBooks()
+        {
+            return Entries.Where((entry) => entry is BibTeXBook);
+        }
+
+        #endregion
+    }
+}
