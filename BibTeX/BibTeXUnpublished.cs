5e095e7c6dd7faef0ce3ebf6831528cd4d58a5d5
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-03-30 20:27:51 +0100

    Added BibTeXUnpublished

---
+++ BibTeX/BibTeXUnpublished.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibTeX
+{
+    [BibTeXEntryName("unpublished")]
+    public class BibTeXUnpublished : BibTeXEntry
+    {
+        [BibTeXFieldName("author")]
+        public string Author { get; set; }
+
+        [BibTeXFieldName("title")]
+        public string Title { get; set; }
+
+        [BibTeXFieldName("year")]
+        [BibTeXOptionalField]
+        public string Year { get; set; }
+
+        [BibTeXFieldName("month")]
+        [BibTeXOptionalField]
+        public BibTeXMonth Month { get; set; }
+
+        public BibTeXUnpublished() { }
+
+        public BibTeXUnpublished(string author, string title)
+        {
+            Author = author;
+            Title = title;
+        }
+    }
+}
