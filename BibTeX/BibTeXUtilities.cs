aea51b9fa9792b83543201c5adb619dcc11a7303
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-04-01 13:48:51 +0100

    Added BibTeXUtilities to make it easier to use the library's main functions

---
+++ BibTeX/BibTeXUtilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibTeX
+{
+    /// <summary>
+    /// A static class that contains the most useful functions of this BibTeX library, such as converting between the object and text forms of a BibTeX databases.
+    /// </summary>
+    public static class BibTeXUtilities
+    {
+        public static string ConvertBibTeXDatabaseToText(BibTeXDatabase database, BibTeXBeginEndFieldValueCharacterType beginEndFieldValueCharacterType = BibTeXBeginEndFieldValueCharacterType.QuotationMarks, BibTeXMonthStyle monthStyle = BibTeXMonthStyle.January, BibTeXFormatStyle formatStyle = BibTeXFormatStyle.Readable)
+        {
+            var serializer = new BibTeXSerializer(beginEndFieldValueCharacterType, monthStyle, formatStyle);
+
+            return serializer.SerializeBibTeXDatabase(database);
+        }
+    }
+}
