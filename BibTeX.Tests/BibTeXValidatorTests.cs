53b7648f1ccbaee73fe9dd776a09fce127d01bd4
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-04-02 18:54:27 +0100

    Added unit tests for the validation

---
+++ BibTeX.Tests/BibTeXValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BibTeX.Tests
+{
+    [TestClass]
+    public class BibTeXValidatorTests
+    {
+        private BibTeXValidator _validator;
+
+        public BibTeXValidatorTests()
+        {
+            _validator = new BibTeXValidator();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RequiredFieldException))]
+        public void ValidateBibTeXEntryFailTest()
+        {
+            var book = new BibTeXBook("", "a", "a", "2000");
+
+            _validator.ValidateBibTeXEntry(book);
+        }
+
+        [TestMethod]
+        public void ValidateBibTeXEntryPassTest()
+        {
+            var book = new BibTeXBook("a", "a", "a", "2000");
+
+            Assert.IsTrue(_validator.ValidateBibTeXEntry(book));
+        }
+    }
+}
