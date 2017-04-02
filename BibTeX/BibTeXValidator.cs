7752d22b7d911858ca7dfac960abe0d0caee09f5
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-04-02 18:13:45 +0100

    Added BibTeXValidator for checking that field values that have been entered into a BibTeX entry or database are allowed

---
+++ BibTeX/BibTeXValidator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace BibTeX
+{
+    public class RequiredFieldException : Exception
+    {
+        public string EntryName { get; }
+        public string Name { get; }
+
+        public RequiredFieldException(string entryName, string name)
+        {
+            EntryName = entryName;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"The field '{Name}' is a required field for '{EntryName}' entries.";
+        }
+    }
+
+    public class BibTeXValidator
+    {
+        protected BibTeXAttributeReader _attributeReader;
+
+        public BibTeXValidator()
+        {
+            _attributeReader = new BibTeXAttributeReader();
+        }
+
+        public bool ValidateBibTeXField(IBibTeXEntry entry, PropertyInfo property)
+        {
+            if (_attributeReader.IsBibTeXFieldRequired(property))
+            {
+                var entryName = _attributeReader.GetBibTeXEntryName(entry);
+                var fieldName = _attributeReader.GetBibTeXFieldName(property);
+                var fieldValue = property.GetValue(entry);
+
+                if (fieldValue is BibTeXMonth)
+                {
+                    if ((BibTeXMonth)fieldValue == BibTeXMonth.None)
+                    {
+                        throw new RequiredFieldException(entryName, fieldName);
+                    }
+                }
+                else
+                {
+                    if (string.IsNullOrWhiteSpace(fieldValue.ToString()))
+                    {
+                        throw new RequiredFieldException(entryName, fieldName);
+                    }
+                }
+            }
+
+            return true;
+        }
+
+
+    }
+}
