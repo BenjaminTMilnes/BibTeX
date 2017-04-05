45a3f8071eeb323ac5643dac33712c17a58413c4
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-04-05 21:42:42 +0100

    Added BibTeXRequiredFieldGroup

---
+++ BibTeX/BibTeXRequiredFieldGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibTeX
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class BibTeXRequiredFieldGroup : Attribute
+    {
+        public string Name { get; }
+
+        public BibTeXRequiredFieldGroup(string name)
+        {
+            Name = name;
+        }
+    }
+}
